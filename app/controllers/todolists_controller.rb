1252b64c865f44f403449e44f4572291cfdb2e04
norioku <fuzisi.okano184@gmail.com>
2019-10-07 17:21:32 +0900

    [Finish2-7]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,24 +1,43 @@
 class TodolistsController < ApplicationController
-  def new
+def new
 
   	@list = List.new
-  end
+end
 
 
-  def create
+def create
 
   	list = List.new(list_params)
 
   	list.save
 
-  	redirect_to '/top'
+  	redirect_to todolist_path(list.id)
 end
+
+def index
+	@todolists = List.all
+end
+
+def show
+	@list = List.find(params[:id])
+	end
+
+def edit
+	@list = List.find(params[:id])
+end
+
+def update
+	list = List.find(params[:id])
+		list.update(list_params)
+		redirect_to todolist_path(list.id)
+end
+
 private
 
 def list_params
 	params.require(:list).permit(:title,:body)
+
+
 end
 
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
 
 	get 'top' => 'root#top'
 
-	post 'todolists' => 'tosolists#creare'
+	post 'todolists' => 'todolists#create'
+
+	get 'todolists' => 'todolists#index'
+
+	get 'todolists/:id' => 'todolists#show',as:'todolist'
+
+	get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+
+	patch 'todolists/:id' => 'todolists#update',as:'update_todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
